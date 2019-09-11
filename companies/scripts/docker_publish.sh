8bfc3b1dc8c88e791f0e4a228f9edded46b85cd4
Alex Vidal <goofyahead@gmail.com>
2019-09-11 11:47:14 +0200

    updated name for tag develop

--- companies/scripts/docker_publish.sh
+++ companies/scripts/docker_publish.sh
@@ -8,7 +8,7 @@ APP_NAME=$1
 MINOR=$(git rev-list HEAD --count --first-parent)
 VERSION=1.0.${MINOR:-1}
 
-docker build . -t "goofyahead/${APP_NAME}:${VERSION}" -t "develop"
+docker build . -t "goofyahead/${APP_NAME}:${VERSION}" -t "goofyahead/${APP_NAME}:develop"
 
 docker push goofyahead/${APP_NAME}:${VERSION}
 docker push goofyahead/${APP_NAME}:develop
