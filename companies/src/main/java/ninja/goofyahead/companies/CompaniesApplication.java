00aeb25ec946334e86e1cc5a53020cbec772d508
Alex Vidal <goofyahead@gmail.com>
2019-09-12 18:22:20 +0200

    show verison when starting

--- companies/src/main/java/ninja/goofyahead/companies/CompaniesApplication.java
+++ companies/src/main/java/ninja/goofyahead/companies/CompaniesApplication.java
@@ -2,19 +2,24 @@ package ninja.goofyahead.companies;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class CompaniesApplication {
 
+    @Autowired
+    private static Environment env;
     private static Logger log = LoggerFactory.getLogger(CompaniesApplication.class);
 
     public static void main (String args []){
+        log.info("Running version: " + env.getProperty("DEPLOYMENT_VERSION"));
         SpringApplication.run(CompaniesApplication.class, args);
     }
 }
