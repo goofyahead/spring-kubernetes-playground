5fd867306723e22c71490d9d542366bd2512ad48
Alex Vidal <goofyahead@gmail.com>
2019-09-12 18:43:14 +0200

    fix get env var for versioning

--- companies/src/main/java/ninja/goofyahead/companies/CompaniesApplication.java
+++ companies/src/main/java/ninja/goofyahead/companies/CompaniesApplication.java
@@ -2,24 +2,20 @@ package ninja.goofyahead.companies;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class CompaniesApplication {
 
-    @Autowired
-    private static Environment env;
     private static Logger log = LoggerFactory.getLogger(CompaniesApplication.class);
 
     public static void main (String args []){
-        log.info("Running version: " + env.getProperty("DEPLOYMENT_VERSION"));
+        log.info("Running version: " + System.getenv("DEPLOYMENT_VERSION"));
         SpringApplication.run(CompaniesApplication.class, args);
     }
 }
